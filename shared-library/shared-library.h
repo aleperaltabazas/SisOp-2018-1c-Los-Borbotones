f2a1b8709850bfa8c2a95b2a516cec8cfd2e4559
utnso <paula.martinezalmiron@gmail.com>
2018-04-21 16:55:03 -0300

    general

---
+++ shared-library/shared-library.h
@@ -0,0 +1,30 @@
+/*
+ * shared-library.h
+ *
+ *  Created on: 21 abr. 2018
+ *      Author: utnso
+ */
+
+#ifndef SHARED_LIBRARY_H_
+#define SHARED_LIBRARY_H_
+
+#include <stdio.h>
+#include <openssl/md5.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <readline/readline.h>
+#include <commons/log.h>
+#include <commons/collections/list.h>
+#include <pthread.h>
+
+t_log * logger;
+
+int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+int client_socket = socket (AF_INET, SOCK_STREAM, 0);
+
+
+
+#endif /* SHARED_LIBRARY_H_ */
