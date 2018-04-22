59374e35a72d7ae381b336ed0a861d942e85d017
Alejandro <aleperaltabazas@hotmail.com>
2018-04-22 04:37:23 -0300

    subo estructura general del protocolo de conexion

--- ESI/esi.c
+++ ESI/esi.c
@@ -8,12 +8,20 @@
 #include "esi.h"
 
 int main(int argv, char** argc){
+	inicializar_log("esi.log", "ESI");
+	char* ip_planificador, puerto_planificador;
+	char* ip_coordinador, puerto_coordinador;
+
+	protocolo_conexion(ip_planificador, puerto_planificador);
+	protocolo_conexion(ip_coordinador, puerto_coordinador);
+
+	//log_info(logger, "ESI: Conexiones ok");
+
 	return EXIT_SUCCESS;
 }
 
 int conectar_a(char *IP, char* puerto){
-
-	struct sockaddr_in direccion_servidor;
+	/*struct sockaddr_in direccion_servidor;
 	direccion_servidor.sin_family = AF_INET;
 	direccion_servidor.sin_addr.s_addr = inet_addr(IP);
 	direccion_servidor.sin_port = htons(puerto);
@@ -26,12 +34,45 @@ int conectar_a(char *IP, char* puerto){
 
 		//Tira error logea el error y exitea
 
-	return socket_cliente;
+	return socket_cliente;*/
 
+	struct addrinfo hints;
+	struct addrinfo *server_info;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo(IP, puerto, &hints, &server_info);
+
+	int server_socket = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+
+	int res = connect(server_socket, server_info->ai_addr, server_info->ai_addrlen);
+
+	freeaddrinfo(server_info);
+
+	if (res < 0) {
+	  salir_con_error("ESI: Fallo la conexion", server_socket);
+	}
+	//log_info(logger, "Conectado!");
+	return server_socket;
+
+}
+
+void protocolo_conexion(char* IP, char* puerto){
+	int socket = conectar_a(IP, puerto);
+	wait_hello(socket);
+	send_hello(socket);
+	void * content = wait_content(socket);
 }
 
 //Por alguna razon los logs no me estan funcionando
 //Si alguno tiene mejor suerte avise
+
+void inicializar_log(char* nombre_archivo, char* nombre_log){
+	//log_create(nombre_archivo, nombre_log, true, LOG_LEVEL_INFO);
+	return;
+}
 
 void salir_con_error(char* mensaje, int socket){
 	//log_error(logger, mensaje);
--- ESI/esi.h
+++ ESI/esi.h
@@ -8,16 +8,24 @@
 #ifndef ESI_H_
 #define ESI_H_
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <openssl/md5.h>
+#include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <unistd.h>
+#include <readline/readline.h>
 #include <commons/log.h>
+#include <commons/collections/list.h>
+#include <pthread.h>
 
 int conectar_a(char* IP, char* puerto);
 void salir_con_error(char* mensaje, int socket);
 void exit_gracefully(int return_val);
 
 t_log * logger;
+
+
 
 #endif /* ESI_H_ */
