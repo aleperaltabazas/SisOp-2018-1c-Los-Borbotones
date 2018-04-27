a66e2b0f4683ead37125033aaa57d42cd2fe1a0a
utnso <ignaciorzeszut97@gmail.com>
2018-04-26 23:46:43 -0300

    Mala mia

--- planificador/planificador.c
+++ planificador/planificador.c
@@ -21,22 +21,35 @@ int main(){
 	hints.ai_flags = AI_PASSIVE;		// Asigna el address del localhost: 127.0.0.1
 	hints.ai_socktype = SOCK_STREAM;	// Indica que usaremos el protocolo TCP
 	getaddrinfo(NULL, PUERTO, &hints, &serverInfo); // Notar que le pasamos NULL como IP, ya que le indicamos que use localhost en AI_PASSIVE
-//Nota: Lean nos dijo que mantengamos esto así por ahora.
+/*Nota: Lean nos dijo que mantengamos esto así por ahora.
+Nota de la nota: Luego de meditarlo, considero que escapa de lo que es operativos en si y como son conexiones pasan
+a ser parte de redes (o eso me gustaría creer)
+*/
 	log_trace(logger, "Nace el planificador");
 	int listenningSocket; //Más adelante creo que vamos a tener que meter esto dentro de un while, con un thread propio para estar "siempre" listo para recibir nuevas conexiones y "reenviarlas"
 	listenningSocket = socket(serverInfo->ai_family, serverInfo->ai_socktype, serverInfo->ai_protocol);
 	bind(listenningSocket,serverInfo->ai_addr, serverInfo->ai_addrlen);
-
-	log_trace(logger, "Conectados!");
-
 	freeaddrinfo(serverInfo);
 
 	listen(listenningSocket, BACKLOG); //En esta parte tendriamos que tener en cuenta un timeout (no recuerdo si era acá que Lean nos dijo que lo tiene incluído)
-	log_trace(logger, "Hable más fuerte que tengo una toalla");
+	log_trace(logger, "Esperando ");
 	struct sockaddr_in addr;			// Esta estructura contendra los datos de la conexion del cliente. IP, puerto, etc.
 	socklen_t addrlen = sizeof(addr);
 	int socketCliente = accept(listenningSocket, (struct sockaddr *) &addr, &addrlen);
+//Nota: Tenemos n sockets address, y vamos a tener que ver como podemos tratar con ellos, se me ocurre threads pero también existe select
+	char package[PACKAGESIZE];
+	int status = 1;		// Estructura que manjea el status de los recieve.
 
+	printf("Cliente conectado. Esperando mensajes:\n");
+	log_trace(logger, "Hable mas fuerte ESI, tengo una toalla"); //Creo que el log es mejor pero en el ejemplito usan printf
 
+	while (status != 0) {
+		status = recv(socketCliente, (void*) package, PACKAGESIZE, 0);
+		if (status != 0) printf("%s", package);
+	}
+	close(socketCliente);
+	close(listenningSocket);
 	return 0;
+	pthread_create ();
 }
+
