f5d4984b63193222e70514b1fa8d6dae465b8e50
Alejandro <aleperaltabazas@hotmail.com>
2018-06-10 16:43:28 -0300

    correcciones en el deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,50 +2,30 @@
 clear
 echo "Comienza el deploy"
 
-sleep 1
-
-cd Coordinador/Debug/
+cd /home/utnso/tp-2018-1c-Los-borbotones/Coordinador/Debug/
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/tp-2018-1c-Los-borbotones/shared-library/Debug
 
 echo "Coordinador listo"
 
-cd ..
-cd ..
-
-sleep 1
-
-cd planificador/Debug/
+cd /home/utnso/tp-2018-1c-Los-borbotones/Planificador/Debug
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/tp-2018-1c-Los-borbotones/shared-library/Debug
 
 echo "Planificador listo"
 
-cd ..
-cd ..
-
-sleep 1
-
-cd ESI/Debug/
+cd /home/utnso/tp-2018-1c-Los-borbotones/ESI/Debug
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/tp-2018-1c-Los-borbotones/shared-library/Debug
 
 echo "ESI listo"
 
-cd ..
-cd ..
-
-sleep 1
-
-cd Instancias/Debug/
+cd /home/utnso/tp-2018-1c-Los-borbotones/Instancias/Debug/
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/tp-2018-1c-Los-borbotones/shared-library/Debug
 
 echo "Instancia lista"
 
-cd ..
-cd ..
-
-sleep 1
+cd /home/utnso/tp-2018-1c-Los-borbotones
 
 echo "Deploy completo"
