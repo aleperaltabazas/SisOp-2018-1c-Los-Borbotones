e032f605b5c98ca27d179a4d92b45df2d5e915bd
aleperaltabazas <aleperaltabazas@hotmail.com>
2018-06-28 23:33:53 -0300

    pokeesi coming soon

---
+++ shared-library/getPokemon.c
@@ -0,0 +1,52 @@
+/*
+ * getPokemon.c
+ *
+ *  Created on: 28 jun. 2018
+ *      Author: utnso
+ */
+
+
+char* getPokemon(uint32_t id){
+	char* pokemon;
+
+	switch(id){
+	case 1:
+		pokemon = "Bulbasaur";
+		break;
+	case 2:
+		pokemon = "Ivysaur";
+		break;
+	case 3:
+		pokemon = "Venusaur";
+		break;
+	case 4:
+		pokemon = "Charmander";
+		break;
+	case 5:
+		pokemon = "Charmeleon";
+		break;
+	case 6:
+		pokemon = "Charizard";
+		break;
+	case 7:
+		pokemon = "Squirtle";
+		break;
+	case 8:
+		pokemon = "Wartorle";
+		break;
+	case 9:
+		pokemon = "Blastoise";
+		break;
+	case 10:
+		pokemon = "Caterpie";
+		break;
+	case 11:
+		pokemon = "Metapod";
+		break;
+	case 12:
+		pokemon = "Butterfree";
+		break;
+	}
+
+	return pokemon;
+}
