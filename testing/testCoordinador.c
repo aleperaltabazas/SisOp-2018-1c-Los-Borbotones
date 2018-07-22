ea32336e8ad749a4547200c32a42aa7554558e3e
Alejandro <aleperaltabazas@hotmail.com>
2018-07-22 18:22:41 -0300

    agrego carpeta de testing y muevo mis tests

---
+++ testing/testCoordinador.c
@@ -0,0 +1,8 @@
+/*
+ * testCoordinador.c
+ *
+ *  Created on: 22 jul. 2018
+ *      Author: alesaurio
+ */
+
+
---
+++ testing/testCoordinador.h
@@ -0,0 +1,13 @@
+/*
+ * testCoordinador.h
+ *
+ *  Created on: 22 jul. 2018
+ *      Author: alesaurio
+ */
+
+#ifndef TESTCOORDINADOR_H_
+#define TESTCOORDINADOR_H_
+
+
+
+#endif /* TESTCOORDINADOR_H_ */
